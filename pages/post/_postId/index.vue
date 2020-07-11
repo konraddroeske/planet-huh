57bd8cfe3685ef61d62771231091111d6cf0f28a
Dan Fitz <dan@danfitz.com>
2020-07-11 14:25:40 -0400

    Created routes for regular and collab posts, added layouts for category and post and error page (#7)

---
+++ pages/post/_postId/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ id }}</div>
+</template>
+
+<script>
+export default {
+  layout: 'post',
+  data() {
+    return {
+      id: this.$route.params.postId,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
