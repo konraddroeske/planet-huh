e3ce990ffae18d06d75e6feaeebddac9c94e0aa6
Dan Fitz <dan@danfitz.com>
2020-08-03 15:35:51 -0400

    Post Feed (#29)
    * Added ability to grab a set number of posts and started PostsFeed component
    
    * Completed mobile view of posts feed
    
    * Minor style changes
    
    * Started styling desktop view of posts feed; added city, sense, mood to query
    
    * Added ability to check if image is in portrait orientation
    
    * Styled post feed
    
    * Increased post title font size
    
    * Decreased margin to bring Load More button closer

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,10 @@
     <Nav />
     <CTA />
     <CategoryNav variant="gradient" />
+    <PostsFeed
+      :posts="postsFeed"
+      :get-some-posts="() => this.$store.dispatch('homepage/getSomePosts', 4)"
+    />
   </div>
 </template>
 
@@ -10,12 +14,19 @@
 import Nav from '@/components/Nav'
 import CTA from '@/components/CTA'
 import CategoryNav from '@/components/CategoryNav'
+import PostsFeed from '@/components/PostsFeed'
 
 export default {
   components: {
     Nav,
     CTA,
     CategoryNav,
+    PostsFeed,
+  },
+  computed: {
+    postsFeed() {
+      return this.$store.state.homepage.postsFeed
+    },
   },
   created() {
     this.$store.dispatch('homepage/getHomepage')
