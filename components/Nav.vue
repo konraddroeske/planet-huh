d551790775b1fdf6ddd8161e6811cf5382818fad
Konrad <konrad@ottosdoner.com>
2020-08-19 20:43:16 -0400

    Nav transitions (#41)
    * added transition to post, need to tie into async call to trigger animation
    
    * sphere animations mostly complete
    
    * fixed pointer events, z-index issues
    
    * page transition between index & post complete
    
    * added event listeners to link back
    
    * removed middleware

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="navContainer">
-    <h1 class="welcome">Welcome To Planet Huh</h1>
-    <div id="sceneContainer" ref="sceneContainer" class="sceneContainer">
-      <canvas ref="scene" class="scene" />
+  <div id="nav3d" class="nav3d">
+    <div id="navContainer" class="navContainer">
+      <h1 class="welcome">Welcome To Planet Huh</h1>
+      <div id="sceneContainer" ref="sceneContainer" class="sceneContainer">
+        <canvas id="scene" ref="scene" class="scene" />
+      </div>
     </div>
-    <div class="toggleContainer">
+    <div id="toggleContainer" class="toggleContainer">
       <NavToggle />
     </div>
   </div>
@@ -135,7 +137,7 @@ export default {
 
       const isMobile = this.$device.isMobile
 
-      const sceneContainer = document.querySelector('#sceneContainer')
+      const sceneContainer = document.querySelector('#navContainer')
 
       let isDragging = false
       let isThrowing = false
@@ -531,12 +533,17 @@ export default {
       // INITIALIZE CANVAS
 
       const canvas = this.$refs.scene
-      const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+      const renderer = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true,
+        alpha: true,
+      })
       renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setClearColor(0x000000, 0)
       // renderer.setSize(window.innerWidth, window.innerHeight)
 
       const scene = new THREE.Scene()
-      scene.background = new THREE.Color('white')
+      // scene.background = new THREE.Color('white')
 
       // CAMERA PIVOT
 
@@ -1418,9 +1425,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.navContainer {
+.nav3d {
   position: relative;
   height: 100vh;
+  // z-index: $z-modal;
+}
+
+.navContainer {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform-origin: bottom right;
+  z-index: $z-modal;
+  cursor: grab;
 }
 
 .welcome {
@@ -1437,8 +1456,9 @@ export default {
 }
 
 .sceneContainer {
+  pointer-events: none;
   text-align: center;
-  cursor: grab;
+
   position: absolute;
   top: 50%;
   left: 50%;
@@ -1452,6 +1472,7 @@ export default {
   height: 152vw;
   display: block;
   pointer-events: none;
+  transition: height 2s;
 }
 
 .toggleContainer {
@@ -1459,7 +1480,7 @@ export default {
   bottom: 5rem;
   left: 50%;
   transform: translate(-50%, 0);
-  z-index: $z-above;
+  z-index: $z-modal;
 }
 
 @media (pointer: none), (pointer: coarse) {
--- pages/post/_slug/index.vue
+++ pages/post/_slug/index.vue
@@ -6,11 +6,94 @@
 </template>
 
 <script>
+import gsap from 'gsap'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 import { fetchContent } from '@/utils/api'
 import HeroBanner from '@/components/HeroBanner'
 import RichText from '@/components/RichText'
+
+gsap.registerPlugin(ScrollToPlugin)
+
+const setNav = () => {
+  console.log('set Nav')
+
+  const navContainer = document.querySelector('#navContainer')
+  const toggle = document.querySelector('#toggleContainer')
+
+  gsap.set(navContainer, {
+    position: 'fixed',
+    y: '-2rem',
+    scale: 0.15,
+  })
+
+  gsap.set('#nav3d', {
+    height: 0,
+  })
+
+  gsap.set(toggle, { autoAlpha: 0 })
+}
+
+const entering = () => {
+  console.log('entering post')
+}
+
+const leaving = () => {
+  console.log('leaving post')
+}
+
+const leavingToIndex = () => {
+  gsap.to('#nav3d', 0.6, {
+    height: '100vh',
+    ease: 'power4.out',
+  })
+
+  const navContainer = document.querySelector('#navContainer')
+  const navContainerTl = gsap.timeline()
+  const scrollTime = 0.3
+  const navTime = 1
+  const delay = 0.2
+
+  navContainerTl
+    .to(window, scrollTime, {
+      scrollTo: 0,
+    })
+    .set('#layout', {
+      height: 'calc(100vh + 1px)',
+      overflow: 'hidden',
+    })
+    .to(navContainer, navTime, {
+      delay,
+      y: '0',
+      scale: 1,
+      ease: 'power4.out',
+    })
+    .set(navContainer, {
+      position: 'absolute',
+    })
+    .set('#layout', {
+      height: 'auto',
+      overflow: 'visible',
+    })
+
+  const toggle = document.querySelector('#toggleContainer')
+  const toggleTl = gsap.timeline()
+  toggleTl.to(toggle, navTime, { autoAlpha: 1 })
+}
+
 export default {
   layout: 'default',
+  transition(to, from) {
+    if (!from) {
+      // set nav to small
+      return setNav()
+    }
+
+    if (from.path === '/') {
+      entering()
+    }
+
+    to.path === '/' ? leavingToIndex() : leaving()
+  },
   components: { HeroBanner, RichText },
   data() {
     return {
@@ -43,6 +126,21 @@ export default {
       imageSrc: data.data.post.coverImage.url,
       content: data.data.post.content.raw.children,
     }
+  },
+  mounted() {
+    const nav = document.querySelector('#navContainer')
+    nav.addEventListener('click', this.route, false)
+  },
+  beforeDestroy() {
+    const nav = document.querySelector('#navContainer')
+    nav.removeEventListener('click', this.route, false)
+  },
+  methods: {
+    route() {
+      this.$router.push({
+        path: `/`,
+      })
+    },
   },
 }
 </script>
