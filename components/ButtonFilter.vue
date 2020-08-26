55dfa925a2058d42363a26b4d8ab87999bee9f30
Konrad Droeske <konrad@ottosdoner.com>
2020-08-25 21:33:28 -0400

    added error handling to queries and fixed navigation to index on mobile

---
+++ components/ButtonFilter.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="buttonFilter" @click="onClick"><slot /></button>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick(event) {
+      this.$emit('clicked')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: 2px solid $black;
+  border-radius: 180px;
+  padding: 0.75rem 1.25rem;
+  background: $black;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: $medium;
+  font-size: $font-sm;
+  color: $white;
+
+  &:hover,
+  &:focus {
+    background: $accent;
+  }
+}
+</style>
