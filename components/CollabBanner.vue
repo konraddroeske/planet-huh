67c06f6b70f2cf87460bbad87af91c7c83ce3457
Dan Fitz <dan@danfitz.com>
2020-08-27 10:27:31 -0400

    Collab banner (#63)
    * Saving work for collab banner
    
    * Done styling mobile
    
    * Styled desktop
    
    Co-authored-by: Dan Fitz <dfitz@freedommobile.ca>

--- components/CollabBanner.vue
+++ components/CollabBanner.vue
@@ -3,13 +3,24 @@
     <div class="wrapper">
       <div class="heroRow">
         <img class="bannerImage" :src="imageSrc" alt="" @load="onImgLoad" />
-        <div class="bannerText">
-          <h1 class="title">{{ title }}</h1>
-          <p class="excerpt">{{ excerpt }}</p>
+        <h1 class="titleText postTitle">
+          <span class="miniTitle">{{ titleArray[0] }}</span>
+          {{ titleArray.slice(1).join(' ') }}
+        </h1>
+
+        <div class="titleText artistOne">
+          {{ artistOne.name }}
+          <span class="miniTitle">{{ artistOne.location }}</span>
+        </div>
+
+        <div class="titleText artistTwo">
+          <span class="miniTitle">{{ artistTwo.location }}</span>
+          {{ artistTwo.name }}
         </div>
       </div>
 
       <div class="metaRow">
+        <p class="accentText headline">{{ headline }}</p>
         <div class="dateContainer">
           <span class="accentText dateTitle">Posted:</span>
           <span class="date"><Date :input="date" /></span>
@@ -19,7 +30,12 @@
           <span class="accentText categoriesTitle">Filed Under:</span>
           <ul class="categories">
             <li v-if="city.length > 0" class="category">
-              <span class="visuallyHidden">City: </span>{{ nameFormatted }}
+              <span class="visuallyHidden">City One: </span
+              >{{ cityOneFormatted }}
+            </li>
+            <li v-if="city.length > 0" class="category">
+              <span class="visuallyHidden">City Two: </span
+              >{{ cityTwoFormatted }}
             </li>
             <li v-if="sense.length > 0" class="category">
               <span class="visuallyHidden">Sense: </span>{{ senseFormatted }}
@@ -47,7 +63,7 @@ export default {
       type: String,
       required: true,
     },
-    excerpt: {
+    headline: {
       type: String,
       required: true,
     },
@@ -55,9 +71,19 @@ export default {
       type: String,
       required: true,
     },
+    artist: {
+      type: Array,
+      required: true,
+      validator(arr) {
+        return arr.length === 2
+      },
+    },
     city: {
       type: Array,
       default: () => [],
+      validator(arr) {
+        return arr.length >= 2
+      },
     },
     sense: {
       type: [Array, String],
@@ -75,8 +101,20 @@ export default {
     moodFormatted() {
       return `${this.mood.mood}`
     },
-    nameFormatted() {
+    cityOneFormatted() {
       return this.city.length > 0 ? `${this.city[0].name}` : null
+    },
+    cityTwoFormatted() {
+      return this.city.length > 0 ? `${this.city[1].name}` : null
+    },
+    artistOne() {
+      return this.artist[0]
+    },
+    artistTwo() {
+      return this.artist[1]
+    },
+    titleArray() {
+      return this.title.split(/\s+/)
     },
   },
   activated() {
@@ -94,12 +132,11 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  margin-top: 5rem;
+  margin-top: 6.5rem;
   text-align: center;
 
   @media (min-width: $bp-desktop) {
-    margin-top: 0;
-    text-align: left;
+    margin-top: 9rem;
   }
 }
 
@@ -108,83 +145,129 @@ export default {
   margin: 0 auto;
 
   @media (min-width: $bp-desktop) {
-    width: 100%;
-    margin: 0;
+    width: 90%;
+    position: relative;
   }
 }
 
 .heroRow {
+  position: relative;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   @media (min-width: $bp-desktop) {
-    display: flex;
-    flex-direction: row-reverse;
-    align-items: center;
-    position: relative;
+    height: 65vh;
   }
 }
 
 .bannerImage {
   display: block;
+  position: absolute;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  height: 100%;
   width: 100%;
-  @media (min-width: $bp-desktop) {
-    width: 50%;
-  }
+  object-fit: cover;
 }
 
-.bannerText {
-  @media (min-width: $bp-desktop) {
-    width: 50%;
-    padding-left: 2rem;
-    padding-right: 5%;
-    padding-top: 1.5rem;
-    padding-bottom: 1.5rem;
-  }
-}
-
-.title {
+.titleText {
   text-transform: uppercase;
+  font-family: $font-display;
   font-weight: $extrabold;
   font-size: 2.25rem;
   @media (min-width: $bp-desktop) {
     font-size: 3.5rem;
-    width: 75%;
-    margin-bottom: 1rem;
   }
 }
 
-.excerpt {
-  font-weight: $semibold;
-  font-size: 1.1rem;
-  margin-bottom: 0;
+.postTitle {
+  position: relative;
+}
+
+.artistOne {
+  position: absolute;
+  top: -1.5rem;
+  left: 0;
+  width: 100%;
+
+  @media (min-width: $bp-desktop) {
+    text-align: left;
+    top: -2rem;
+    left: -1.5rem;
+    width: calc(100% + 3rem);
+  }
+}
+
+.artistTwo {
+  position: absolute;
+  bottom: -1.5rem;
+  right: 0;
+  width: 100%;
+
+  @media (min-width: $bp-desktop) {
+    text-align: right;
+    bottom: -2rem;
+    right: -1.5rem;
+    width: calc(100% + 3rem);
+  }
+}
+
+.miniTitle {
+  display: block;
+  font-weight: $medium;
+  font-size: 1.15rem;
   @media (min-width: $bp-desktop) {
     font-size: 1.5rem;
   }
 }
 
-.metaRow {
+.headline {
+  color: $gray;
+  font-weight: $medium;
+  font-size: 1.1rem;
+  margin: 4rem 0 2rem;
   @media (min-width: $bp-desktop) {
-    display: flex;
-    width: calc(100% - 4rem);
+    font-family: $font-display;
+    font-weight: $semibold;
+    font-size: 1.25rem;
+    color: $black;
+    text-transform: none;
+    margin: 6rem 0 4rem;
+  }
+}
+
+.metaRow {
+  margin: 0 0 3rem;
+  @media (min-width: $bp-desktop) {
     margin: 2rem 2rem 0 2rem;
   }
 }
 
 .dateContainer {
   @media (min-width: $bp-desktop) {
-    width: 50%;
+    position: absolute;
+    top: calc(65vh + 1rem);
+    left: 0;
   }
 }
 
 .categoriesContainer {
   @media (min-width: $bp-desktop) {
-    width: 50%;
     display: flex;
+    position: absolute;
+    right: 0;
+    top: -2.5rem;
   }
 }
 
 .dateTitle,
 .categoriesTitle {
   display: block;
-  margin-top: 2rem;
+  margin-top: 2.5rem;
 
   @media (min-width: $bp-desktop) {
     display: inline-block;
