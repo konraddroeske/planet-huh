1d63b48258412f47463ce4a8c64d1400f0163ee7
Konrad Droeske <konrad@ottosdoner.com>
2020-09-10 16:21:26 -0400

    removed excess static page functions

--- store/static.js
+++ store/static.js
@@ -27,27 +27,6 @@ export const actions = {
       console.log(error) // TODO: error handling
     }
   },
-  async getStaticPage({ commit }, slug) {
-    try {
-      const { data } = await fetchContent(`{
-        staticPage(where: {slug: "${slug}"}) {
-          title
-          content {
-            raw
-          }
-        }
-      }`)
-
-      const staticPage = {
-        title: data.data.staticPage.title.trim(),
-        content: data.data.staticPage.content.raw.children,
-      }
-      commit("setStaticPage", { staticPage, slug })
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error) // TODO: error handling
-    }
-  },
   async getStaticPages({ commit, state }) {
     try {
       const { data } = await fetchContent(`{
@@ -75,12 +54,6 @@ export const actions = {
 export const mutations = {
   setStaticSlugs(state, slugs) {
     state.slugs = slugs
-  },
-  setStaticPage(state, { staticPage, slug }) {
-    state.staticPages = {
-      ...state.staticPages,
-      [slug]: staticPage,
-    }
   },
   setPages(state, payload) {
     state.pages = payload
