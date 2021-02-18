194286d573c3d63ff95a47b24762161b34a82ad3
Konrad Droeske <kdroeske@gmail.com>
2021-02-18 11:12:49 -0500

    updated target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,12 +27,12 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: "universal",
+  // mode: "universal",
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: "server",
+  target: "static",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
