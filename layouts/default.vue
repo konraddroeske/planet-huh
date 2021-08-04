e1624e7665aa8aaaa761e6bd2b54eaf78ce08457
Konrad <kdroeske@gmail.com>
2021-08-04 13:21:17 -0400

    Development (#133)
    * disabled 3d nav
    
    * add mood submission prompt when user enters page (#132)
    
    * add mood submission prompt when user enters page
    
    * fix scroll on mood submission compoenent, add margin to load more button, route to home after submission
    
    * readded 3d animation
    
    Co-authored-by: Konrad Droeske <kdroeske@gmail.com>
    
    Co-authored-by: atina <56001902+whoisasey@users.noreply.github.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,52 @@
 <template>
   <div id="layout" ref="layout" class="layout">
-    <Loading />
-    <ModalFilters v-if="modal" />
-    <Header />
-    <div id="blurrableContent" class="burrableContent">
-      <Nav />
-      <main id="main" class="main">
-        <Nuxt keep-alive />
-      </main>
+    <div v-if="saved === null">
+      <MoodSubmission />
+    </div>
+    <div v-else>
+      <Loading />
+      <ModalFilters v-if="modal" />
+      <Header />
+      <div id="blurrableContent" class="burrableContent">
+        <Nav />
+        <main id="main" class="main">
+          <Nuxt keep-alive />
+        </main>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex"
+import Vue from "vue"
+
+import VueCookies from "vue-cookies"
 import Header from "@/components/Header"
 import Nav from "@/components/Nav"
 import Loading from "@/components/Loading"
+import MoodSubmission from "@/components/MoodSubmission"
+
+Vue.use(VueCookies)
 
 export default {
-  components: { Header, Nav, Loading },
+  components: {
+    Header,
+    Nav,
+    Loading,
+    MoodSubmission,
+  },
+
   computed: {
     ...mapState({
       modal: (state) => state.categories.modal,
+      saved: (state) => state.saved,
     }),
     isMobile() {
       return this.$device.isMobileOrTablet
     },
   },
+
   mounted() {
     this.$store.commit("transitions/setMobile", this.isMobile)
   },
